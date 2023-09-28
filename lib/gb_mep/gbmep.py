1de800f8f77abb0ccd7f7c3b0d7f3c7f7f4c5ca7
fraspass <fraspass@outlook.com>
2023-09-28 18:29:00 +0100

    fixed single node models

--- lib/gb_mep/gbmep.py
+++ lib/gb_mep/gbmep.py
@@ -17,7 +17,7 @@ class gb_mep:
         else:
             # Store DataFrame
             self.df = df
-            # Find unique nodes (node IDs should start at 0 and match the dictionary and ID mapping dictionary)
+            # Find unique nodes (node IDs should start at 0 with maximum index M-1 and match the dictionary and ID mapping dictionary)
             self.nodes = np.unique(self.df[['start_id','end_id']].values)
             # For each node, find all start times and end times
             self.start_times = {}
@@ -58,28 +58,23 @@ class gb_mep:
                 condition_full = True
                 condition = not (distance_start or distance_end)
             if condition_full:
-                for secondary_node in ([node] if condition else np.where(ds < thresh)[0]):
-                    if self.N[secondary_node] + self.N_prime[secondary_node] > 0:
-                        start_breaks = np.searchsorted(a=self.start_times[secondary_node], v=self.start_times[node], side='left')
-                        end_breaks = np.searchsorted(a=self.end_times[secondary_node], v=self.start_times[node], side='left')
-                        for k, t in enumerate(self.start_times[node]):
-                            if k > 0:
-                                time_diffs_A[k, secondary_node] = t - self.start_times[secondary_node][start_breaks[k-1]:start_breaks[k]]
-                                time_diffs_A_prime[k, secondary_node] = t - self.end_times[secondary_node][end_breaks[k-1]:end_breaks[k]]
-                            else:
-                                time_diffs_A[k, secondary_node] = t - self.start_times[secondary_node][:start_breaks[k]]
-                                time_diffs_A_prime[k, secondary_node] = t - self.end_times[secondary_node][:end_breaks[k]]
-                    else:
-                        for k, t in enumerate(self.start_times[node]):
-                            time_diffs_A[k, secondary_node] = np.array([])
-                            time_diffs_A_prime[k, secondary_node] = np.array([])
+                for secondary_node in ([node] if condition else np.intersect1d(ar1=self.nodes, ar2=np.where(ds < thresh)[0])):
+                    start_breaks = np.searchsorted(a=self.start_times[secondary_node], v=self.start_times[node], side='left')
+                    end_breaks = np.searchsorted(a=self.end_times[secondary_node], v=self.start_times[node], side='left')
+                    for k, t in enumerate(self.start_times[node]):
+                        if k > 0:
+                            time_diffs_A[k, secondary_node] = t - self.start_times[secondary_node][start_breaks[k-1]:start_breaks[k]]
+                            time_diffs_A_prime[k, secondary_node] = t - self.end_times[secondary_node][end_breaks[k-1]:end_breaks[k]]
+                        else:
+                            time_diffs_A[k, secondary_node] = t - self.start_times[secondary_node][:start_breaks[k]]
+                            time_diffs_A_prime[k, secondary_node] = t - self.end_times[secondary_node][:end_breaks[k]]
             # Obtain the correct log-likelihood function based on fitting parameters
             if start_times and end_times and distance_start and distance_end:
                 f = self.negative_loglikelihood_full
-                f_args = (node, time_diffs_A, time_diffs_A_prime, np.where(ds < thresh)[0])
+                f_args = (node, time_diffs_A, time_diffs_A_prime, thresh)
             elif start_times and end_times and distance_start and not distance_end:
                 f = self.negative_loglikelihood_full_start
-                f_args = (node, time_diffs_A, time_diffs_A_prime, np.where(ds < thresh)[0])
+                f_args = (node, time_diffs_A, time_diffs_A_prime, thresh)
             elif start_times and end_times and not distance_start and not distance_end:
                 f = self.negative_loglikelihood_smep
                 f_args = (node, time_diffs_A, time_diffs_A_prime)
@@ -171,62 +166,71 @@ class gb_mep:
         return -ll
 
     ### Calculate negative log-likelihood for the full model without a distance function for the end times, for a specific node index
-    def negative_loglikelihood_full_start(self, p, node_index, time_diffs_A, time_diffs_A_prime, subset_nodes):
+    def negative_loglikelihood_full_start(self, p, node_index, time_diffs_A, time_diffs_A_prime, thresh=1):
         # Transform parameters to original scale (lambda, alpha, beta, theta, alpha_prime, beta_prime, theta_prime)
         params = np.exp(p)
         params[2] += params[1]
         params[5] += params[4]
         # Time differences for starting times for node with corresponding index
         time_diffs = np.diff(self.start_times[node_index])
-        # Pre-define arrays for the recursive terms (A and A_prime)
-        A = np.zeros((len(time_diffs)+1,self.M))
-        A_prime = np.zeros((len(time_diffs)+1,self.M))
         # Obtain distance between node and all other nodes
         ds = self.distance_matrix[node_index]
+        if thresh is None:
+            subset_nodes = self.nodes
+        else:
+            subset_nodes = np.intersect1d(ar1=self.nodes, ar2=np.where(ds < thresh)[0])
+        # Pre-define arrays for the recursive terms (A and A_prime)
+        A = np.zeros((len(time_diffs)+1, len(subset_nodes)))
+        A_prime = np.zeros((len(time_diffs)+1, len(subset_nodes)))
         # Compensator component of loglikelihood
         ll = -params[0] * self.T
         # Loop over all nodes
-        for node in subset_nodes:
+        for nn, node in enumerate(subset_nodes):
             # Compensator components of loglikelihood
             ll += np.exp(-params[3] * ds[node]) * params[1] / params[2] * np.sum(np.exp(-params[2] * (self.T - self.start_times[node])) - 1)
-            ll += np.exp(-params[6] * ds[node]) * params[4] / params[5] * np.sum(np.exp(-params[5] * (self.T - self.end_times[node])) - 1)
+            if node == node_index:
+                ll += params[4] / params[5] * np.sum(np.exp(-params[5] * (self.T - self.end_times[node])) - 1)
             # Loop over all events and update recursive terms A and A_prime
             for k, _ in enumerate(self.start_times[node_index]):    
-                A[k, node] = ((np.exp(-params[2] * time_diffs[k-1]) * A[k-1, node]) if k > 0 else 0) + np.sum(np.exp(-params[2] * time_diffs_A[k, node]))
+                A[k, nn] = ((np.exp(-params[2] * time_diffs[k-1]) * A[k-1, nn]) if k > 0 else 0) + np.sum(np.exp(-params[2] * time_diffs_A[k, node]))
                 if node == node_index:
-                    A_prime[k, node] = ((np.exp(-params[5] * time_diffs[k-1]) * A_prime[k-1, node]) if k > 0 else 0) + np.sum(np.exp(-params[5] * time_diffs_A_prime[k, node])) 
+                    A_prime[k, nn] = ((np.exp(-params[5] * time_diffs[k-1]) * A_prime[k-1, nn]) if k > 0 else 0) + np.sum(np.exp(-params[5] * time_diffs_A_prime[k, node])) 
         # Calculate B and use it to update the log-likelihood
-        B = np.exp(-params[3] * ds) * params[1] * A + params[4] * A_prime
+        B = np.exp(-params[3] * ds[subset_nodes]) * params[1] * A + params[4] * A_prime
         ll += np.sum(np.log(params[0] + B.sum(axis=1)))
         # Return final value
         return -ll
 
     ### Calculate negative log-likelihood for the full model, for a specific node index
-    def negative_loglikelihood_full(self, p, node_index, time_diffs_A, time_diffs_A_prime, subset_nodes):
+    def negative_loglikelihood_full(self, p, node_index, time_diffs_A, time_diffs_A_prime, thresh=None):
         # Transform parameters to original scale (lambda, alpha, beta, theta, alpha_prime, beta_prime, theta_prime)
         params = np.exp(p)
         params[2] += params[1]
         params[5] += params[4]
         # Time differences for starting times for node with corresponding index
         time_diffs = np.diff(self.start_times[node_index])
-        # Pre-define arrays for the recursive terms (A and A_prime)
-        A = np.zeros((len(time_diffs)+1,self.M))
-        A_prime = np.zeros((len(time_diffs)+1,self.M))
         # Obtain distance between node and all other nodes
         ds = self.distance_matrix[node_index]
+        if thresh is None:
+            subset_nodes = self.nodes
+        else:
+            subset_nodes = np.intersect1d(ar1=self.nodes, ar2=np.where(ds < thresh)[0])
+        # Pre-define arrays for the recursive terms (A and A_prime)
+        A = np.zeros((len(time_diffs)+1, len(subset_nodes)))
+        A_prime = np.zeros((len(time_diffs)+1, len(subset_nodes)))
         # Compensator component of loglikelihood
         ll = -params[0] * self.T
         # Loop over all nodes
-        for node in subset_nodes:
+        for nn, node in enumerate(subset_nodes):
             # Compensator components of loglikelihood
             ll += np.exp(-params[3] * ds[node]) * params[1] / params[2] * np.sum(np.exp(-params[2] * (self.T - self.start_times[node])) - 1)
             ll += np.exp(-params[6] * ds[node]) * params[4] / params[5] * np.sum(np.exp(-params[5] * (self.T - self.end_times[node])) - 1)
             # Loop over all events and update recursive terms A and A_prime
             for k, _ in enumerate(self.start_times[node_index]):    
-                A[k, node] = ((np.exp(-params[2] * time_diffs[k-1]) * A[k-1, node]) if k > 0 else 0) + np.sum(np.exp(-params[2] * time_diffs_A[k, node])) 
-                A_prime[k, node] = ((np.exp(-params[5] * time_diffs[k-1]) * A_prime[k-1, node]) if k > 0 else 0) + np.sum(np.exp(-params[5] * time_diffs_A_prime[k, node])) 
+                A[k, nn] = ((np.exp(-params[2] * time_diffs[k-1]) * A[k-1, nn]) if k > 0 else 0) + np.sum(np.exp(-params[2] * time_diffs_A[k, node])) 
+                A_prime[k, nn] = ((np.exp(-params[5] * time_diffs[k-1]) * A_prime[k-1, nn]) if k > 0 else 0) + np.sum(np.exp(-params[5] * time_diffs_A_prime[k, node])) 
         # Calculate B and use it to update the log-likelihood
-        B = np.exp(-params[3] * ds) * params[1] * A + np.exp(-params[6] * ds) * params[4] * A_prime
+        B = np.exp(-params[3] * ds[subset_nodes]) * params[1] * A + np.exp(-params[6] * ds[subset_nodes]) * params[4] * A_prime
         ll += np.sum(np.log(params[0] + B.sum(axis=1)))
         # Return final value
         return -ll
@@ -287,4 +291,43 @@ class gb_mep:
         baseline_terms = -params[0] * np.insert(arr=time_diffs, obj=0, values=self.start_times[node_index][0])
         A_terms = params[1] / params[2] * np.insert(arr=np.diff(A)-1, obj=0, values=A[0])
         A_prime_terms = params[3] / params[4] * (np.insert(arr=np.diff(A_prime), obj=0, values=A_prime[0]) - end_breaks_diff)
+        return np.exp(baseline_terms + A_terms + A_prime_terms)
+    
+    ## Calculate p-values for GB-MEP with distance function
+    def pvals_gbmep(self, params, node_index, thresh=None):
+        # Time differences for starting times for node with corresponding index
+        time_diffs = np.diff(self.start_times[node_index])
+        # Pre-define arrays for the recursive terms (A and A_prime)
+        A = np.zeros((len(time_diffs)+1,self.M))
+        A_prime = np.zeros((len(time_diffs)+1,self.M))
+        # Obtain distance between node and all other nodes
+        ds = self.distance_matrix[node_index]
+        # Find the subset of nodes based on the difference 
+        if thresh is None:
+            subset_nodes = self.nodes
+        else:
+            subset_nodes = np.intersect1d(ar1=self.nodes, ar2=np.where(ds < thresh)[0])
+        # Calculate baseline terms for p-values
+        baseline_terms = -params[0] * np.insert(arr=time_diffs, obj=0, values=self.start_times[node_index][0])
+        A_terms = np.zeros(len(A)); A_prime_terms = np.zeros(len(A_prime))
+        # Calculate required elements for recursion
+        for secondary_node in subset_nodes:
+            start_breaks = np.searchsorted(a=self.start_times[secondary_node], v=self.start_times[node_index], side='left')
+            start_breaks_diff = np.insert(arr=np.diff(start_breaks), obj=0, values=start_breaks[0])
+            end_breaks = np.searchsorted(a=self.end_times[secondary_node], v=self.start_times[node_index], side='left')
+            end_breaks_diff = np.insert(arr=np.diff(end_breaks), obj=0, values=end_breaks[0])
+            for k, t in enumerate(self.start_times[node_index]):
+                if k > 0:
+                    time_diffs_A = t - self.start_times[secondary_node][start_breaks[k-1]:start_breaks[k]]
+                    time_diffs_A_prime = t - self.end_times[secondary_node][end_breaks[k-1]:end_breaks[k]]
+                else:
+                    time_diffs_A = t - self.start_times[secondary_node][:start_breaks[k]]
+                    time_diffs_A_prime = t - self.end_times[secondary_node][:end_breaks[k]]
+                ## Update A and A_prime
+                A[k] = ((np.exp(-params[2] * time_diffs[k-1]) * A[k-1, secondary_node]) if k > 0 else 0) + np.sum(np.exp(-params[2] * time_diffs_A)) 
+                A_prime[k] = ((np.exp(-params[4] * time_diffs[k-1]) * A_prime[k-1, secondary_node]) if k > 0 else 0) + np.sum(np.exp(-params[4] * time_diffs_A_prime)) 
+            ## Update A and A_prime terms for calculation of differences between conpensators
+            A_terms += np.exp(-params[3] * ds[secondary_node]) * params[1] / params[2] * (np.insert(arr=np.diff(A), obj=0, values=A[0]) - start_breaks_diff)
+            A_prime_terms += np.exp(-params[6] * ds[secondary_node]) * params[4] / params[5] * (np.insert(arr=np.diff(A_prime), obj=0, values=A_prime[0]) - end_breaks_diff)
+        ## Return p-values
         return np.exp(baseline_terms + A_terms + A_prime_terms)

75262d5e04d6a421c7c5040cb409215f5e19ca97
fraspass <fraspass@outlook.com>
2023-10-17 12:03:26 +0100

    added results

--- scripts/process_results_gbmep.py
+++ scripts/process_results_gbmep.py
@@ -34,6 +34,18 @@ with open('results/res_smep.pkl', 'rb') as f:
     res_smep = pickle.load(f)
 
 ## Load results for the GB-MEP model without start times only
+res_gbmep_start_shared = {}
+for file in glob.glob('results/res_gbmep_start_shared/*.pkl'):
+    with open(file, 'rb') as f:
+        res_gbmep_start_shared.update(pickle.load(f))
+
+## Load results for the GB-MEP model
+res_gbmep_shared = {}
+for file in glob.glob('results/res_gbmep_shared/*.pkl'):
+    with open(file, 'rb') as f:
+        res_gbmep_shared.update(pickle.load(f))
+
+## Load results for the GB-MEP model without start times only
 res_gbmep_start = {}
 for file in glob.glob('results/res_gbmep_start/*.pkl'):
     with open(file, 'rb') as f:
@@ -50,16 +62,16 @@ G = gb_mep.gb_mep(df=santander_train, id_map=santander_dictionary, distance_matr
 start_times, end_times = G.augment_start_times(santander_test)
 
 ## Initialise dictionaries for observed quantiles
-y_poisson_train = {}; y_poisson_test = {}; y_mep_train = {}; y_mep_test = {}; y_sep_train = {}; y_sep_test = {}
-y_smep_train = {}; y_smep_test = {}; y_gbmep_start_train = {}; y_gbmep_start_test = {}; y_gbmep_train = {}; y_gbmep_test = {}
+y_poisson_train = {}; y_poisson_test = {}; y_mep_train = {}; y_mep_test = {}; y_sep_train = {}; y_sep_test = {}; y_smep_train = {}; y_smep_test = {}
+y_gbmep_start_shared_train = {}; y_gbmep_start_shared_test = {}; y_gbmep_shared_train = {}; y_gbmep_shared_test = {}; y_gbmep_start_train = {}; y_gbmep_start_test = {}; y_gbmep_train = {}; y_gbmep_test = {}
 
 ## Intialise dictionaries for KS scores
-ks_poisson_train = {}; ks_mep_train = {}; ks_sep_train = {}; ks_smep_train = {}; ks_gbmep_start_train = {}; ks_gbmep_train = {}
-ks_poisson_test = {}; ks_mep_test = {}; ks_sep_test = {}; ks_smep_test = {}; ks_gbmep_start_test = {}; ks_gbmep_test = {}
+ks_poisson_train = {}; ks_mep_train = {}; ks_sep_train = {}; ks_smep_train = {}; ks_gbmep_start_shared_train = {}; ks_gbmep_shared_train = {}; ks_gbmep_start_train = {}; ks_gbmep_train = {}
+ks_poisson_test = {}; ks_mep_test = {}; ks_sep_test = {}; ks_smep_test = {}; ks_gbmep_start_shared_test = {}; ks_gbmep_shared_test = {}; ks_gbmep_start_test = {}; ks_gbmep_test = {}
 
 ## Intialise dictionaries for Cramér-von Mises scores
-cvm_poisson_train = {}; cvm_mep_train = {}; cvm_sep_train = {}; cvm_smep_train = {}; cvm_gbmep_start_train = {}; cvm_gbmep_train = {}
-cvm_poisson_test = {}; cvm_mep_test = {}; cvm_sep_test = {}; cvm_smep_test = {}; cvm_gbmep_start_test = {}; cvm_gbmep_test = {}
+cvm_poisson_train = {}; cvm_mep_train = {}; cvm_sep_train = {}; cvm_smep_train = {}; cvm_gbmep_start_shared_train = {}; cvm_gbmep_shared_train = {}; cvm_gbmep_start_train = {}; cvm_gbmep_train = {}
+cvm_poisson_test = {}; cvm_mep_test = {}; cvm_sep_test = {}; cvm_smep_test = {}; cvm_gbmep_start_shared_test = {}; cvm_gbmep_shared_test = {}; cvm_gbmep_start_test = {}; cvm_gbmep_test = {}
 
 ## Define theoretical quantiles
 x = np.linspace(start=0, stop=1, num=501, endpoint=False)[1:]
@@ -80,6 +92,12 @@ for index in G.nodes:
     pp = gb_mep.transform_parameters(res_smep[index].x)
     p_smep_train, p_smep_test = G.pvals_smep(params=pp, node_index=index, start_times=start_times, end_times=end_times, test_split=True)
     # p-values for GB-MEP with start times only
+    pp  = gb_mep.transform_parameters(res_gbmep_start_shared[index].x)
+    p_gbmep_start_shared_train, p_gbmep_start_shared_test = G.pvals_gbmep_start_shared(params=pp, node_index=index, subset_nodes=res_gbmep_start_shared[index].subset_nodes, start_times=start_times, test_split=True)
+    # p-values for GB-MEP
+    pp = gb_mep.transform_parameters(res_gbmep_shared[index].x)
+    p_gbmep_shared_train, p_gbmep_shared_test = G.pvals_gbmep_start_self_shared(params=pp, node_index=index, subset_nodes=res_gbmep_shared[index].subset_nodes, start_times=start_times, end_times=end_times, test_split=True) 
+    # p-values for GB-MEP with start times only
     pp  = gb_mep.transform_parameters(res_gbmep_start[index].x)
     p_gbmep_start_train, p_gbmep_start_test = G.pvals_gbmep_start(params=pp, node_index=index, subset_nodes=res_gbmep_start[index].subset_nodes, start_times=start_times, test_split=True)
     # p-values for GB-MEP
@@ -90,6 +108,8 @@ for index in G.nodes:
     y_mep_train[index] = np.percentile(a=p_mep_train, q=x*100)
     y_sep_train[index] = np.percentile(a=p_sep_train, q=x*100)
     y_smep_train[index] = np.percentile(a=p_smep_train, q=x*100)
+    y_gbmep_start_shared_train[index] = np.percentile(a=p_gbmep_start_shared_train, q=x*100)
+    y_gbmep_shared_train[index] = np.percentile(a=p_gbmep_shared_train, q=x*100)
     y_gbmep_start_train[index] = np.percentile(a=p_gbmep_start_train, q=x*100)
     y_gbmep_train[index] = np.percentile(a=p_gbmep_train, q=x*100)
     # Caclulate observed percentiles for test set
@@ -98,6 +118,8 @@ for index in G.nodes:
         y_mep_test[index] = np.percentile(a=p_mep_test, q=x*100)
         y_sep_test[index] = np.percentile(a=p_sep_test, q=x*100)
         y_smep_test[index] = np.percentile(a=p_smep_test, q=x*100)
+        y_gbmep_start_shared_test[index] = np.percentile(a=p_gbmep_start_shared_test, q=x*100)
+        y_gbmep_shared_test[index] = np.percentile(a=p_gbmep_shared_test, q=x*100)
         y_gbmep_start_test[index] = np.percentile(a=p_gbmep_start_test, q=x*100)
         y_gbmep_test[index] = np.percentile(a=p_gbmep_test, q=x*100)
     # Calculate KS statistic for training set
@@ -105,6 +127,8 @@ for index in G.nodes:
     ks_mep_train[index] = stats.kstest(p_mep_train, stats.uniform.cdf)
     ks_sep_train[index] = stats.kstest(p_sep_train, stats.uniform.cdf)
     ks_smep_train[index] = stats.kstest(p_smep_train, stats.uniform.cdf)
+    ks_gbmep_start_shared_train[index] = stats.kstest(p_gbmep_start_shared_train, stats.uniform.cdf)
+    ks_gbmep_shared_train[index] = stats.kstest(p_gbmep_shared_train, stats.uniform.cdf)
     ks_gbmep_start_train[index] = stats.kstest(p_gbmep_start_train, stats.uniform.cdf)
     ks_gbmep_train[index] = stats.kstest(p_gbmep_train, stats.uniform.cdf)
     # Calculate KS statistic for test set
@@ -113,6 +137,8 @@ for index in G.nodes:
         ks_mep_test[index] = stats.kstest(p_mep_test, stats.uniform.cdf)
         ks_sep_test[index] = stats.kstest(p_sep_test, stats.uniform.cdf)
         ks_smep_test[index] = stats.kstest(p_smep_test, stats.uniform.cdf)
+        ks_gbmep_start_shared_test[index] = stats.kstest(p_gbmep_start_shared_test, stats.uniform.cdf)
+        ks_gbmep_shared_test[index] = stats.kstest(p_gbmep_shared_test, stats.uniform.cdf)
         ks_gbmep_start_test[index] = stats.kstest(p_gbmep_start_test, stats.uniform.cdf)
         ks_gbmep_test[index] = stats.kstest(p_gbmep_test, stats.uniform.cdf)
     # Calculate Cramér-von Mises statistic for training set
@@ -120,6 +146,8 @@ for index in G.nodes:
     cvm_mep_train[index] = np.sqrt(stats.cramervonmises(rvs=p_mep_train, cdf=stats.uniform.cdf).statistic / G.N[index])
     cvm_sep_train[index] = np.sqrt(stats.cramervonmises(rvs=p_sep_train, cdf=stats.uniform.cdf).statistic / G.N[index])
     cvm_smep_train[index] = np.sqrt(stats.cramervonmises(rvs=p_smep_train, cdf=stats.uniform.cdf).statistic / G.N[index])
+    cvm_gbmep_start_shared_train[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_start_shared_train, cdf=stats.uniform.cdf).statistic / G.N[index])
+    cvm_gbmep_shared_train[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_shared_train, cdf=stats.uniform.cdf).statistic / G.N[index])
     cvm_gbmep_start_train[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_start_train, cdf=stats.uniform.cdf).statistic / G.N[index])
     cvm_gbmep_train[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_train, cdf=stats.uniform.cdf).statistic / G.N[index])
     # Calculate Cramér-von Mises for test set
@@ -128,24 +156,26 @@ for index in G.nodes:
         cvm_mep_test[index] = np.sqrt(stats.cramervonmises(rvs=p_mep_test, cdf=stats.uniform.cdf).statistic / G.N[index])
         cvm_sep_test[index] = np.sqrt(stats.cramervonmises(rvs=p_sep_test, cdf=stats.uniform.cdf).statistic / G.N[index])
         cvm_smep_test[index] = np.sqrt(stats.cramervonmises(rvs=p_smep_test, cdf=stats.uniform.cdf).statistic / G.N[index])
+        cvm_gbmep_start_shared_test[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_start_shared_test, cdf=stats.uniform.cdf).statistic / G.N[index])
+        cvm_gbmep_shared_test[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_shared_test, cdf=stats.uniform.cdf).statistic / G.N[index])
         cvm_gbmep_start_test[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_start_test, cdf=stats.uniform.cdf).statistic / G.N[index])
         cvm_gbmep_test[index] = np.sqrt(stats.cramervonmises(rvs=p_gbmep_test, cdf=stats.uniform.cdf).statistic / G.N[index])
 
 ## Save the results
 y_train = {}; ks_train = {}; cvm_train = {}
 y_test = {}; ks_test = {}; cvm_test = {}
-y_train['poisson'] = y_poisson_train; y_train['mep'] = y_mep_train; y_train['sep'] = y_sep_train
-y_train['smep'] = y_smep_train; y_train['gbmep_start'] = y_gbmep_start_train; y_train['gbmep'] = y_gbmep_train
-ks_train['poisson'] = ks_poisson_train; ks_train['mep'] = ks_mep_train; ks_train['sep'] = ks_sep_train
-ks_train['smep'] = ks_smep_train; ks_train['gbmep_start'] = ks_gbmep_start_train; ks_train['gbmep'] = ks_gbmep_train
-cvm_train['poisson'] = cvm_poisson_train; cvm_train['mep'] = cvm_mep_train; cvm_train['sep'] = cvm_sep_train
-cvm_train['smep'] = cvm_smep_train; cvm_train['gbmep_start'] = cvm_gbmep_start_train; cvm_train['gbmep'] = cvm_gbmep_train
-y_test['poisson'] = y_poisson_test; y_test['mep'] = y_mep_test; y_test['sep'] = y_sep_test
-y_test['smep'] = y_smep_test; y_test['gbmep_start'] = y_gbmep_start_test; y_test['gbmep'] = y_gbmep_test
-ks_test['poisson'] = ks_poisson_test; ks_test['mep'] = ks_mep_test; ks_test['sep'] = ks_sep_test
-ks_test['smep'] = ks_smep_test; ks_test['gbmep_start'] = ks_gbmep_start_test; ks_test['gbmep'] = ks_gbmep_test
-cvm_test['poisson'] = cvm_poisson_test; cvm_test['mep'] = cvm_mep_test; cvm_test['sep'] = cvm_sep_test
-cvm_test['smep'] = cvm_smep_test; cvm_test['gbmep_start'] = cvm_gbmep_start_test; cvm_test['gbmep'] = cvm_gbmep_test
+y_train['poisson'] = y_poisson_train; y_train['mep'] = y_mep_train; y_train['sep'] = y_sep_train; y_train['smep'] = y_smep_train
+y_train['gbmep_start_shared'] = y_gbmep_start_shared_train; y_train['gbmep_shared'] = y_gbmep_shared_train; y_train['gbmep_start'] = y_gbmep_start_train; y_train['gbmep'] = y_gbmep_train
+ks_train['poisson'] = ks_poisson_train; ks_train['mep'] = ks_mep_train; ks_train['sep'] = ks_sep_train; ks_train['smep'] = ks_smep_train
+ks_train['gbmep_start_shared'] = ks_gbmep_start_shared_train; ks_train['gbmep_shared'] = ks_gbmep_shared_train; ks_train['gbmep_start'] = ks_gbmep_start_train; ks_train['gbmep'] = ks_gbmep_train
+cvm_train['poisson'] = cvm_poisson_train; cvm_train['mep'] = cvm_mep_train; cvm_train['sep'] = cvm_sep_train; cvm_train['smep'] = cvm_smep_train
+cvm_train['gbmep_start_shared'] = cvm_gbmep_start_shared_train; cvm_train['gbmep_shared'] = cvm_gbmep_shared_train; cvm_train['gbmep_start'] = cvm_gbmep_start_train; cvm_train['gbmep'] = cvm_gbmep_train
+y_test['poisson'] = y_poisson_test; y_test['mep'] = y_mep_test; y_test['sep'] = y_sep_test; y_test['smep'] = y_smep_test
+y_test['gbmep_start_shared'] = y_gbmep_start_shared_test; y_test['gbmep_shared'] = y_gbmep_shared_test; y_test['gbmep_start'] = y_gbmep_start_test; y_test['gbmep'] = y_gbmep_test
+ks_test['poisson'] = ks_poisson_test; ks_test['mep'] = ks_mep_test; ks_test['sep'] = ks_sep_test; ks_test['smep'] = ks_smep_test
+ks_test['gbmep_start_shared'] = ks_gbmep_start_shared_test; ks_test['gbmep_shared'] = ks_gbmep_shared_test; ks_test['gbmep_start'] = ks_gbmep_start_test; ks_test['gbmep'] = ks_gbmep_test
+cvm_test['poisson'] = cvm_poisson_test; cvm_test['mep'] = cvm_mep_test; cvm_test['sep'] = cvm_sep_test; cvm_test['smep'] = cvm_smep_test
+cvm_test['gbmep_start_shared'] = cvm_gbmep_start_shared_test; cvm_test['gbmep_shared'] = cvm_gbmep_shared_test; cvm_test['gbmep_start'] = cvm_gbmep_start_test; cvm_test['gbmep'] = cvm_gbmep_test
 
 ## Check if directory exists
 if not os.path.exists('results/res_qq_start'):
